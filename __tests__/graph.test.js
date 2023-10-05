63f7afa7eca75d9d4304f3e47825a07b65abba40
Eusebie Siebenberg <siebenee@gmail.com>
2023-10-04 19:15:06 -0700

    test pass, correctly adds node, and checks if node exists or not

--- __tests__/graph.test.js
+++ __tests__/graph.test.js
@@ -10,4 +10,18 @@ describe("Graph", () => {
   test("should correctly instantiate a graph", () => {
     expect(graph.adjacencyList.size).toEqual(0);
   });
+
+  test("should add a new node", () => {
+    graph.addNode("Jasmine");
+    expect(graph.adjacencyList.get("Jasmine").size).toEqual(0);
+  });
+
+  test("should return false if the node does not exit in the adjacency list", () => {
+    expect(graph.hasNode("Ada")).toEqual(false);
+  });
+
+  test("check to see if node exists in graph", () => {
+    graph.addNode("Jasmine");
+    expect(graph.hasNode("Jasmine")).toEqual(true);
+  });
 });
--- src/graph.js
+++ src/graph.js
@@ -2,4 +2,15 @@ export default class Graph {
   constructor() {
     this.adjacencyList = new Map();
   }
+
+  addNode(name) {
+    this.adjacencyList.set(name, new Set());
+  }
+
+  hasNode(name) {
+    if (this.adjacencyList.get(name)) {
+      return true;
+    }
+    return false;
+  }
 }
