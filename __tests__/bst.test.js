e08777e3e76a8d297caab8211e09516f056340ae
Eusebie Siebenberg <siebenee@gmail.com>
2023-10-04 18:56:49 -0700

    add new graph test, src files, 1st test pass to create new graph

--- __tests__/bst.test.js
+++ __tests__/bst.test.js
@@ -75,3 +75,34 @@ test("it should not add duplicate nodes", () => {
     root: { data: 36, left: null, right: null },
   });
 });
+
+const pbst = new BST();
+
+beforeEach(() => {
+  pbst.insertNode(new BSTNode(4));
+  pbst.insertNode(new BSTNode(2));
+  pbst.insertNode(new BSTNode(6));
+  pbst.insertNode(new BSTNode(1));
+  pbst.insertNode(new BSTNode(3));
+  pbst.insertNode(new BSTNode(5));
+  pbst.insertNode(new BSTNode(7));
+});
+
+test("it should return true if the root node is equal to 4", () => {
+  expect(pbst.search(4)).toEqual(true);
+});
+
+test("it should return false if the root node is not equal to 31", () => {
+  expect(pbst.search(31)).toEqual(false);
+});
+
+test("it should return if the value 0 is not in the tree", () => {
+  expect(pbst.search(0)).toEqual(false);
+});
+test("it should return true if the tree includes 7", () => {
+  expect(pbst.search(7)).toEqual(true);
+});
+
+test("it should return true if the tree includes 5", () => {
+  expect(pbst.search(5)).toEqual(true);
+});
---
+++ __tests__/graph.test.js
@@ -0,0 +1,13 @@
+import Graph from "../src/graph.js";
+
+describe("Graph", () => {
+  let graph = new Graph();
+
+  afterEach(() => {
+    graph = new Graph();
+  });
+
+  test("should correctly instantiate a graph", () => {
+    expect(graph.adjacencyList.size).toEqual(0);
+  });
+});
