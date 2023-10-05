30267931c6912590bb2b6ae784da14e214900a7c
Eusebie Siebenberg <siebenee@gmail.com>
2023-10-04 19:46:35 -0700

    add ability to remove edge and remove node, test pass

--- __tests__/graph.test.js
+++ __tests__/graph.test.js
@@ -29,7 +29,30 @@ describe("Graph", () => {
     graph.addNode("Jasmine");
     graph.addNode("Ada");
     graph.createEdge("Jasmine", "Ada");
-    expect(graph.adjacencyList.get("Jasmine").has("Ada")).toEqual(true);
-    expect(graph.adjacencyList.get("Ada").has("Jasmine")).toEqual(true);
+    expect(graph.hasEdge("Ada", "Jasmine")).toEqual(true);
+  });
+
+  test("check to see if edge exists in graph", () => {
+    graph.addNode("Jasmine");
+    graph.addNode("Ada");
+    expect(graph.hasEdge("Jasmine", "Ada")).toEqual(false);
+  });
+  test("remove an edge between two nodes", () => {
+    graph.addNode("Jasmine");
+    graph.addNode("Ada");
+    graph.createEdge("Jasmine", "Ada");
+    graph.removeEdge("Jasmine", "Ada");
+    expect(graph.hasEdge("Ada", "Jasmine")).toEqual(false);
+  });
+  test("should delete a node and all of its adjacent nodes", () => {
+    graph.addNode("Ada");
+    graph.addNode("Jasmine");
+    graph.addNode("Lydia");
+    graph.createEdge("Ada", "Jasmine");
+    graph.createEdge("Ada", "Lydia");
+    graph.removeNode("Ada");
+    expect(graph.hasNode("Ada")).toEqual(false);
+    expect(graph.hasEdge("Jasmine", "Ada")).toEqual(false);
+    expect(graph.hasEdge("Lydia", "Ada")).toEqual(false);
   });
 });
